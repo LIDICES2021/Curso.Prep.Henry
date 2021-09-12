8aaf97579b6b4f3a756902c484f345272cf37d45
LIDICES2021 <lidices.reyes@gmail.com>
2021-09-12 16:26:04 -0400

    actualizando

--- 04-JS-III/homework/ej.js
+++ 04-JS-III/homework/ej.js
@@ -1,19 +1,20 @@
 
+var frases = ['hola esto string', 'frase con palabra'];
 
-const lista = ("lidices")
-function crearGato(nombre, edad) {
-  // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
-  // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
-  // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
-  // Devuelve el objeto
-  // Tu código:
-  var obj = {
-      nombre : nombre,
-      edad : edad,
-      meow : function (){
-        return "Meow!";
-    }
-  };
-  return obj;
-} 
-console.log(crearGato(lista))
+
+function palabraMasLarga(array) {
+ var masLarga = "";
+ for(var j = 0; j < array.length; j++) {
+    var palabras = array[j].split(' ');
+    var longitud = masLarga.length;  
+      for(var i = 0; i < palabras.length; i++){      
+        if(palabras[i].length > longitud){
+          longitud = palabras[i].length;
+          masLarga = palabras[i];
+        }
+      }
+  }
+  return masLarga
+}
+
+console.log(palabraMasLarga(frases));
